6857a3343f35ed037de369fa39839bdc4b2f43a0
Lesly Alvarez <leslyalvarez08@gmail.com>
2022-07-14 21:56:30 -0500

    Initial commit- Javascript Challenge

---
+++ script.js
@@ -0,0 +1,43 @@
+function isNumberPositive(num) {
+
+    if (Math.sign(num) === 1) {
+        return true;
+      }
+    
+      return false;
+
+    }
+
+    console.log(isNumberPositive(3));
+    console.log(isNumberPositive(-3));
+
+    
+
+function convertDaysToAge(num) {
+
+        let years = num / 365
+     
+        return years;
+      }
+      
+      console.log(convertDaysToAge(7300));
+
+
+console.log(Math.max(1, 3, 2));
+
+const getLastName = ['Chris', 'Eric', 'Ash', 'Yajis']
+ console.log (lastItem = getLastName[getLastName.length - 1])
+
+
+function isNumberPositive(num) {
+
+    if (Math.sign(num) === 1) {
+        return true;
+      }
+    
+      return false;
+
+    }
+
+    console.log(allNumbersPositive(3,5,6));
+    console.log(allNumbersPositive(-3,5,6));
